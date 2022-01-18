adb483ce04795ba1481e11adf6491badb48daada
bsh6463 <bsh9510@gmail.com>
2022-01-19 00:05:33 +0900

    220118 Json to Object

--- src/main/java/hello/commute/api/SubPath.java
+++ src/main/java/hello/commute/api/SubPath.java
@@ -1,4 +1,41 @@
 package hello.commute.api;
 
+import java.util.List;
+
 public class SubPath {
+
+    private int trafficType;
+    private double distance;
+    private int sectionTime;
+    private int stationCount;
+    private List<Lane> laneList;
+    private String startName;
+    private double startX;
+    private double startY;
+    private String endName;
+    private double endX;
+    private double endY;
+    private String way;
+    private int wayCode;
+    private String door;
+    private int startID;
+    private int endID;
+
+
+    public static class Lane{
+        private String name;
+        private String busNo;
+        private int type;
+        private int busID;
+        private int subwayCode;
+        private int subwayCityCode;
+        private List<PassStop> passStopList;
+    }
+
+    public static class PassStop{
+        private String stationName;
+        private String x;
+        private String y;
+        private String isNonStop;
+    }
 }
